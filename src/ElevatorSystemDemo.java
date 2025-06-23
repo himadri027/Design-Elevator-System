d31aaec3c17eb17f0d6dabb924baf71a23a0c3db
Himadri Vaghela <himadrivaghela1107@gmail.com>
2025-06-23 12:12:22 +0530

    added all the files

---
+++ src/ElevatorSystemDemo.java
@@ -0,0 +1,10 @@
+public class ElevatorSystemDemo {
+    public static void run() {
+        ElevatorController controller = new ElevatorController(3, 5);
+
+        controller.requestElevator(5, 10);
+        controller.requestElevator(3, 7);
+        controller.requestElevator(8, 2);
+        controller.requestElevator(1, 9);
+    }
+}
